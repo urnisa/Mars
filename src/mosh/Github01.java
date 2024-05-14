9e2ba76597dabca5800298b69af34592a6de53ef
Bekbolot E <bekoxna@gmail.com>
2024-05-14 22:11:13 +0600

    commit in branch feauture1

--- src/mosh/Github01.java
+++ src/mosh/Github01.java
@@ -5,6 +5,7 @@ public class Github01 {
         System.out.println("Github01");
         System.out.println("Github02");
         System.out.println("Github03");
+        System.out.println("created in branch feature1");
 
     }
 }
