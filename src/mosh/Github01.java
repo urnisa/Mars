708d35454886523a33053712ac09fed43177b2ec
Bekbolot E <bekoxna@gmail.com>
2024-05-14 19:34:23 +0600

    Added 'Github03' line to project

--- src/mosh/Github01.java
+++ src/mosh/Github01.java
@@ -4,5 +4,7 @@ public class Github01 {
     public static void main(String[] args) {
         System.out.println("Github01");
         System.out.println("Github02");
+        System.out.println("Github03");
+
     }
 }
